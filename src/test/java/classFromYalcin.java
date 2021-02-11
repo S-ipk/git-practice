1f56c6c1594e5cac964a5b4bee9a073f8239cd2d
YalcinKetenci <yalcinketencischool@gmail.com>
2021-02-11 18:30:10 +0200

    My first commit

---
+++ src/test/java/classFromYalcin.java
@@ -0,0 +1,6 @@
+public class classFromYalcin {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
--- src/test/java/confilict.java
+++ src/test/java/confilict.java
@@ -9,5 +9,6 @@ public class confilict {
 
         //line 5
 
+
     }
 }
